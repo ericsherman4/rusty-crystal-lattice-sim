2953257704d499fafe2cb0dbe497bdd91d905c3c
Eric Sherman <esherman2018@gmail.com>
2024-05-19 12:26:55 -0400

    oh wow format is cool

--- sim/src/main.rs
+++ sim/src/main.rs
@@ -7,12 +7,10 @@ fn main() {
         .run()
 }
 
-
-
 mod introduce {
 
     // ECS system
-    // system is a normal rust function 
+    // system is a normal rust function
     // a component is rust structs that implement the Component trait
     // #[derive(Component)]
     // entities are a simple type containing a unique number
@@ -24,16 +22,18 @@ mod introduce {
         fn build(&self, app: &mut App) {
             app.insert_resource(GreetTimer(Timer::from_seconds(2_f32, TimerMode::Repeating)))
                 .add_systems(Startup, add_people)
-                // systems runs in default whenever possible! 
+                // systems runs in default whenever possible!
                 // can add .chain() on the tuple if we want to enforce a particular order the functions run in
-                .add_systems(Startup, (hello_world, (greet_people, greet_personless).chain()))
-                .add_systems(Update,clock_tick);
-
+                .add_systems(
+                    Startup,
+                    (hello_world, (greet_people, greet_personless).chain()),
+                )
+                .add_systems(Update, clock_tick);
         }
     }
 
     // an example of a system (normal rust functions)
-    fn hello_world(){
+    fn hello_world() {
         println!("hello world");
     }
 
@@ -42,7 +42,6 @@ mod introduce {
         commands.spawn(Person); // no name
         commands.spawn(Name("I am not a person".to_string()));
     }
-
 
     // paremeters we pass in to a system function define what data the system runs on
     // runs on all entities with the person and the name component
@@ -59,8 +58,7 @@ mod introduce {
         }
     }
 
-    fn clock_tick(time: Res<Time>, mut timer: ResMut<GreetTimer>)
-    {
+    fn clock_tick(time: Res<Time>, mut timer: ResMut<GreetTimer>) {
         if timer.0.tick(time.delta()).just_finished() {
             println!("hi {}", time.delta().as_millis());
         }
@@ -70,15 +68,9 @@ mod introduce {
     #[derive(Component)]
     struct Person;
 
-
     #[derive(Component)]
     struct Name(String);
 
-
-
     #[derive(Resource)]
     struct GreetTimer(Timer);
-
 }
-
-
