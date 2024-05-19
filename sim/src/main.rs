08a36d63bcdefe290dfd2aacaa4c2f869766d3af
Eric Sherman <esherman2018@gmail.com>
2024-05-19 17:54:30 -0400

    cleanup and an attempt to reset camera with space

--- sim/src/main.rs
+++ sim/src/main.rs
@@ -1,17 +1,18 @@
 use bevy::prelude::*;
+
+// https://docs.rs/smooth-bevy-cameras/0.11.0/smooth_bevy_cameras/
+// https://github.com/bonsairobo/smooth-bevy-cameras/blob/main/examples/simple_unreal.rs
+use smooth_bevy_cameras::LookTransformPlugin;
 use smooth_bevy_cameras::controllers::unreal::{UnrealCameraBundle, UnrealCameraController, UnrealCameraPlugin};
-use smooth_bevy_cameras::{LookTransformPlugin};
 
 fn main() {
     App::new()
         // plugins are pretty cool and enforces modular. if you don't want it, just remove it!
         .add_plugins((DefaultPlugins,LookTransformPlugin ,UnrealCameraPlugin::default()))
         .add_systems(Startup, setup)
-        // .add_systems(Update, smooth_bevy_cameras::controllers::unreal::control_system)
-        // .add_systems(Update, move_camera_system)
+        .add_systems(Update,keyboard_input)
         .run()
 }
-
 
 
 // throw some basic shapes in a 3d environment
@@ -38,7 +39,7 @@ fn setup(
 
     commands.spawn(point_light_bundle);
 
-    // 
+
     commands.spawn(PbrBundle {
         mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
         material: materials.add(Color::GREEN),
@@ -60,35 +61,39 @@ fn setup(
         ..default()
     });
 
-    // let bevy_camera = Camera3dBundle {
-    //     projection: PerspectiveProjection {
-    //         ..default()
-    //     }.into(),
-    //     // looking at is how to orient
-    //     // y is up in bevy
-    //     transform: Transform::from_xyz(-6.0, 10.0, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
-    //     ..default()
-    // };
-
-
-    // let look_transform = LookTransformBundle {
-    //     transform: LookTransform::new(Vec3::new(-6.0, 10.0, 9.0), Vec3::default(), Vec3::Y),
-    //     smoother: Smoother::new(0.9), // Value between 0.0 and 1.0, higher is smoother.
-    // };
+    let bevy_camera = Camera3dBundle {
+        projection: PerspectiveProjection {
+            ..default()
+        }.into(),
+        // looking at is how to orient
+        // y is up in bevy
+        transform: Transform::from_xyz(-6.0, 10.0, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    };
 
     let unreal_camera = UnrealCameraBundle::new(
         UnrealCameraController::default(),
-        Vec3::new(-2.0, 5.0, 5.0),
+        Vec3::new(-6.0, 10.0, 9.0),
         Vec3::new(0., 0., 0.),
         Vec3::Y,
     ); 
 
-
-    commands.spawn(Camera3dBundle::default()).insert(unreal_camera);
+    commands.spawn((MyCamera,bevy_camera)).insert(unreal_camera);
 
 }
 
-// fn move_camera_system(mut cameras: Query<&mut LookTransform>) {
-//     // Later, another system will update the `Transform` and apply smoothing automatically.
-//     for mut c in cameras.iter_mut() { c.target += Vec3::new(0.001, 0.001, 0.001); }
-// }
+
+// this doesnt work idk lol
+// not sure if the library gives us the ability to do this
+// probably write own in the future and get rid of the unreal camera controls you dont want
+fn keyboard_input(keys: Res<ButtonInput<KeyCode>>, mut query: Query<&mut Transform, With<MyCamera>>) {
+    if keys.just_pressed(KeyCode::Space){
+        for mut trans in &mut query {
+            println!("reseting camera!");
+            *trans = Transform::from_xyz(-6.0, 10.0, 9.0);
+        }
+    }
+}
+
+#[derive(Component)]
+struct MyCamera;
