0dcb271f9abfd02455ad847e9d11bcce89e16641
Eric Sherman <esherman2018@gmail.com>
2024-05-19 20:22:41 -0400

    cleanup and creating axes

--- sim/src/main.rs
+++ sim/src/main.rs
@@ -1,4 +1,8 @@
+
 use bevy::prelude::*;
+
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
 
 // https://docs.rs/smooth-bevy-cameras/0.11.0/smooth_bevy_cameras/
 // https://github.com/bonsairobo/smooth-bevy-cameras/blob/main/examples/simple_unreal.rs
@@ -14,19 +18,12 @@ fn main() {
         .run()
 }
 
-
-// throw some basic shapes in a 3d environment
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    // Sphere
-
+fn create_light(commands: &mut Commands)
+{
     // Light
     let point_light = PointLight {
         shadows_enabled: true,
-        intensity: 2_000_000.0,
+        intensity: 1_300_000.0,
         range: 100.0,
         ..default()
     };
@@ -38,48 +35,112 @@ fn setup(
     };
 
     commands.spawn(point_light_bundle);
+}
 
 
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
-        material: materials.add(Color::GREEN),
-        transform: Transform::from_xyz(0.0, 2.0, 0.0),
+#[derive(EnumIter)]
+enum Axis {
+    X,
+    Y,
+    Z,
+}
+
+// maybe can make a custom mesh to solve this but this is easier
+// https://www.christopherbiscardi.com/why-do-bevy-sprites-spawn-with-the-center-at-0-0
+// fn set_cuboid_pos_by_end(target_position: Vec3, length_in_dir: f32, dir: CuboidOffsetDir) -> Vec3
+// { 
+//     // By default, it sets it based on origin. 
+//     // Returns a modified transform that will place the cuboid correctly.
+//     match dir {
+//         CuboidOffsetDir::Y => Vec3::new(target_position.x, target_position.y + length_in_dir/2.0, target_position.z),
+//         _ => Vec3::default(),
+//     }
+// }
+
+fn create_axis(direction: Axis, meshes: &mut ResMut<Assets<Mesh>>, materials:&mut ResMut<Assets<StandardMaterial>>) -> PbrBundle {
+
+    const GIRTH: f32 = 0.2;
+    const LENGTH: f32 = 5.;
+
+    let cuboid_dim: Vec3;
+    let adjusted_position:Vec3;
+    let color: Color;
+
+    match direction {
+        Axis::X => {
+            cuboid_dim = Vec3::new(LENGTH, GIRTH, GIRTH);
+            adjusted_position = Vec3::new(LENGTH/2., 0., 0.);
+            color = Color::RED;
+        },
+        Axis::Y => {
+            cuboid_dim = Vec3::new(GIRTH, LENGTH, GIRTH);
+            adjusted_position = Vec3::new(0., LENGTH/2., 0.);
+            color= Color::GREEN;
+        },
+        Axis::Z => {
+            cuboid_dim = Vec3::new(GIRTH, GIRTH, LENGTH);
+            adjusted_position = Vec3::new(0., 0., LENGTH/2.);
+            color= Color::BLUE;
+        },
+    }
+
+    PbrBundle {
+        mesh: meshes.add(Cuboid::from_size(cuboid_dim)),
+        material: materials.add(color),
+        transform: Transform::from_translation(adjusted_position),
+        ..default()
+    }
+
+}
+
+fn create_coord_system(commands: &mut Commands, meshes:&mut ResMut<Assets<Mesh>>,materials: &mut ResMut<Assets<StandardMaterial>>) 
+{
+    // Origin
+    commands.spawn( PbrBundle {
+        mesh: meshes.add(Sphere::default().mesh().uv(32,18)),
+        material: materials.add(Color::WHITE),
+        transform: Transform::from_translation(Vec3::ZERO),
         ..default()
     });
 
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
-        material: materials.add(Color::RED),
-        transform: Transform::from_xyz(2.0, 0.0, 0.0),
-        ..default()
-    });
+    for variant in Axis::iter() {
+        commands.spawn(create_axis(variant, meshes, materials));
+    }
+ }
 
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
-        material: materials.add(Color::BLUE),
-        transform: Transform::from_xyz(0.0, 0.0, 2.0),
-        ..default()
-    });
-
+ fn create_cameras(commands : &mut Commands)
+ {
     let bevy_camera = Camera3dBundle {
         projection: PerspectiveProjection {
             ..default()
         }.into(),
         // looking at is how to orient
         // y is up in bevy
-        transform: Transform::from_xyz(-6.0, 10.0, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     };
 
     let unreal_camera = UnrealCameraBundle::new(
         UnrealCameraController::default(),
-        Vec3::new(-6.0, 10.0, 9.0),
+        Vec3::new(-2.5, 4.5, 9.0),
         Vec3::new(0., 0., 0.),
         Vec3::Y,
     ); 
 
     commands.spawn((MyCamera,bevy_camera)).insert(unreal_camera);
+ }
 
+
+// throw some basic shapes in a 3d environment
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+
+    create_light(&mut commands);    
+    create_coord_system(&mut commands, &mut meshes,&mut  materials);
+    create_cameras(&mut commands);
 }
 
 
@@ -90,7 +151,7 @@ fn keyboard_input(keys: Res<ButtonInput<KeyCode>>, mut query: Query<&mut Transfo
     if keys.just_pressed(KeyCode::Space){
         for mut trans in &mut query {
             println!("reseting camera!");
-            *trans = Transform::from_xyz(-6.0, 10.0, 9.0);
+            *trans = Transform::from_xyz(-2.5, 4.5, 9.0);
         }
     }
 }
