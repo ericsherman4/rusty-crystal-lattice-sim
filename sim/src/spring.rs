2a012b3c9771dc4c3b7ba8f6562aef0a81cb5750
Eric Sherman <esherman2018@gmail.com>
2024-07-28 17:19:54 -0400

    idk some random stuff from long time ago

--- sim/src/spring.rs
+++ sim/src/spring.rs
@@ -1,22 +1,56 @@
 use bevy::{ecs::query, prelude::*};
 
 #[derive(Component)]
-pub struct Link{
-    to_node: &Node,
-    from_node: &Node,
-}
-
-#[derive(Component)]
 pub struct Node
 {
     pos: Vec3,
     vel: Vec3,
     sum_forces: Vec3,
-    
+    mesh: Mesh,
+}
+
+impl Node {
+    fn new(pos: Vec3, vel: Vec3, rad: f32) -> Self {
+        Node {
+            pos,
+            vel,
+            sum_forces : Vec3::ZERO,
+            mesh: Sphere::new(rad).mesh().uv(32,18),
+        }
+    }
 }
 
 #[derive(Component)]
-pub struct Static;
+pub struct Link<'a>{
+    // don't think we need a pos as the pos is determined by the 2 nodes.
+    spring_const: f32,
+    orig_length: f32,
+    to_node: &'a Node,
+    from_node: &'a Node,
+    mesh: Mesh, 
+}
+
+impl<'a> Link<'a>{
+    fn new(spring_const: f32, orig_length: f32, girth: f32, to_node : &'a Node, from_node: &'a Node ) -> Self {
+        Link {
+            spring_const,
+            orig_length,
+            to_node,
+            from_node,
+
+            // we shall use x as the length and then we will functions to rotate and move accordingly.
+            mesh : Cuboid::new(orig_length, girth, girth).mesh(),
+        }
+    }
+}
+
+// used on Nodes that are anchored
+// or you you can just have this as a flag in the node? 
+// kind of interesting to do because it fully changes the calculation
+// can maybe think of it as infinite mass? 
+// #[derive(Component)]
+// pub struct Static;
+
 
 
 
@@ -25,52 +59,69 @@ pub fn create_spring(
     meshes: &mut ResMut<Assets<Mesh>>, 
     materials: &mut ResMut<Assets<StandardMaterial>>
 ) {
-
     let sphere_rad:f32  = 0.3; 
-    let link_girth: f32 = 0.2;
-    let pos_node1 = Vec3::new(0., 0., 5.);
+    let pos_node1 = Vec3::new(0., 0., 5.); 
     let pos_node2 = Vec3::new(5., 0., 5.);
-    let link = Vec3::new(2.5, 0., 5.); //placed at center
+    let vel_node1 = Vec3::new(0.,0.0,0.); 
+    let vel_node2 = Vec3::new(0.1,0.1,0.1);
 
-    let node1 = PbrBundle {
-        mesh : meshes.add(Sphere::new(sphere_rad).mesh().uv(32,18)),
-        material: materials.add(Color::YELLOW),
-        transform: Transform::from_translation(pos_node1),
-        ..default()
-    };
+    let node1 = Node::new(pos_node1, vel_node1, sphere_rad);
+    let node2 = Node::new(pos_node2, vel_node2, sphere_rad);
 
-    let node2 = PbrBundle {
-        mesh : meshes.add(Sphere::new(sphere_rad).mesh().uv(32,18)),
-        material: materials.add(Color::YELLOW),
-        transform: Transform::from_translation(pos_node2),
-        ..default()
-    };
+    let link = Link::new(2., 5., 0.2, &node2, &node1);
 
-    let node1_data = Node {
-        pos : pos_node1,
-        vel: Vec3::new(0.,0.0,0.),
-        sum_forces: Vec3::ZERO,
-    };
+    // i create a node and then give a reference to it to link but after this function, node is dropped. or rather
+    // its ownership is transferred to bundled.
 
-    let node2_data = Node {
-        pos : pos_node2,
-        vel: Vec3::new(0.1,0.1,0.1),
-        sum_forces: Vec3::ZERO,
-    };
+    // https://github.com/bevyengine/bevy/discussions/13309
+    // https://docs.rs/bevy/latest/bevy/ecs/component/trait.Component.html
+    // in above link, components must always satisfy 'static trait bound.
 
-    // Create two nodes separated apart on the x axis
-    commands.spawn((node1, node1_data, Static));
-    commands.spawn((node2, node2_data));
+    // ECS = entity component and system.
+    // this creates an entity i.e object with the given components.
+    // entities are just unique IDs or references that point to the components. 
+    // see this video, think you might be going at this from an OOP perspective and not an ECS perspective. 
+    // https://www.youtube.com/watch?v=B6ZFuYYZCSY
+    // DATA in COMPONENTs, BEHAVIOR in SYSTEMs.
+    // there is some kind of id function and maybe we just loop through and check the ID of the node.
+    // seems super slow though but apparently the .get method has constant time complexity.
+    // https://gamedev.stackexchange.com/questions/204007/in-bevy-ecs-what-is-a-good-way-to-have-entities-reference-each-other
 
-    // create a skinny box between them
-    let link = PbrBundle { 
-        mesh: meshes.add(Cuboid::from_size(Vec3::new(5.0, link_girth, link_girth))),
-        material: materials.add(Color::YELLOW),
-        transform: Transform::from_translation(link),
-        ..default()
-    };
+    commands.spawn(
+        (
+            PbrBundle {
+                mesh: meshes.add(node1.mesh.clone()), // not ideal
+                material: materials.add(Color::YELLOW),
+                transform: Transform::from_translation(node1.pos.clone()),
+                ..default()
+            },
+            node1 
+        )
+    );
 
-    commands.spawn((link, Link{to_node: &node1_data, from_node: &node2_data}));
+    commands.spawn(
+        (
+            PbrBundle {
+                mesh: meshes.add(node2.mesh.clone()), 
+                material: materials.add(Color::YELLOW),
+                transform: Transform::from_translation(node2.pos.clone()),
+                ..default()
+            },
+            node2
+        )
+    );
+
+    commands.spawn(
+        (
+            PbrBundle { 
+                mesh: meshes.add(link.mesh.clone()),
+                material: materials.add(Color::YELLOW),
+                transform: Transform::from_translation(node2.pos - node1.pos),
+                ..default()
+            },
+            link
+        )
+    );
 
 
 }
@@ -80,12 +131,12 @@ pub fn create_spring(
 // a = F /m
 // v = v_i + a * dt
 // x = x_i + v * dt
-pub fn animate_spring(time: Res<Time>, mut query: Query<&mut Transform, With<Node>>) {
+// pub fn animate_spring(time: Res<Time>, mut query: Query<&mut Transform, With<Node>>) {
     
 
     
-    for mut transform in &mut query {
+//     for mut transform in &mut query {
 
-    }
+//     }
 
-}
+// }
