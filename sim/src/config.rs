a2e107461b30217acc9a6bf32daeb955793d9275
Eric Sherman <esherman2018@gmail.com>
2024-12-08 18:59:41 -0500

    attempting fixes, created dope bug

--- sim/src/config.rs
+++ sim/src/config.rs
@@ -9,18 +9,22 @@ pub mod colors_config {
 }
 
 pub mod lattice_config {
+    use bevy::prelude::Visibility;
+
     pub const DIM: u32 = 7;
     pub const LINK_RADIUS: f32 = 0.1; //x and y component of cuboid get this
-    pub const STARTING_LINK_LEN: f32 = 2.; // z component of cuboid gets this
+    pub const STARTING_LINK_LEN: f32 = 4.; // z component of cuboid gets this
+    pub const LINK_VISIBILITY: Visibility = Visibility::Visible;
 
     pub const NODE_RADIUS: f32 = 0.1 / 2.;
     pub const NODE_MASS: f32 = 10.0;
 
     pub const SPRING_CONST: f32 = 2.0;
 
-    const START_VEL_ABS: f32 = 0.1;
+    const START_VEL_ABS: f32 = 0.5;
     pub const START_VEL_MIN: f32 = -START_VEL_ABS;
     pub const START_VEL_MAX: f32 = START_VEL_ABS;
+
 }
 
 pub mod lights_config {
