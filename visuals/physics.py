52b65c6301a3a3a1c17c8f3516bf70b27129e3a8
Eric Sherman <esherman2018@gmail.com>
2025-02-09 17:19:36 -0500

    bunch of progress writing the equations

--- visuals/physics.py
+++ visuals/physics.py
@@ -58,10 +58,9 @@ class InfluenceDiagram(Scene):
         self.play(group.animate.shift(group.get_top() - new_group.get_top()))
         self.wait(0.5)
         self.play(Write(spring_stuff_mob))
+        self.remove(spring_stuff) #remove the original since its now part of the group (i think)
         self.replace(group, new_group)
         self.wait(1)
-
-
 
         # Transform "spring stuff into spring displacement"
         spring_displacement_str = r"{{Spring}}\;Displacement"
@@ -69,7 +68,6 @@ class InfluenceDiagram(Scene):
         spring_displacement= MathTex(spring_displacement_str, font_size=35).move_to(spring_stuff.get_center())
         self.play(Transform(spring_stuff_mob, spring_displacement))
         self.wait(0.5)
-
 
 
         def zero_but_comp(comp, point3d):
@@ -108,18 +106,22 @@ class InfluenceDiagram(Scene):
         self.play(Create(complete_arrow), run_time=4)
         self.wait(1)
 
+        # Make a group with everything
+        new_group += complete_arrow
+
+
         # transform all the words into functions
         # the lists are reversed because the order of the strings in the vgroup are opposite.
         # also its a lot easier to pop instead of keeping track of indexes
         items.reverse()
-        corresponding_math_symbols = [r"\Delta \vec{x}", r"\vec{F}", r"\vec{a}", r"\vec{v}", r"\vec{x}"]
+        corresponding_math_symbols = [r"\Delta x", r"\vec{F}", r"\vec{a}", r"\vec{v}", r"\vec{x}"]
         corresponding_math_symbols.reverse()
         animations = []
         for i in range(0,len(new_group),2):
             component_first_word = items.pop().split(r"\;")[0]
-            latex = component_first_word + r"\;" + corresponding_math_symbols.pop()
+            latex = r"{{" + component_first_word + r"\;" + corresponding_math_symbols.pop() + r"}}"
             transform_into = MathTex(latex, font_size=35).move_to(new_group[i].get_center())
-            animations.append(Transform(new_group[i], transform_into))
+            animations.append(ReplacementTransform(new_group[i], transform_into))
 
 
         self.play(*animations) # play rewording everything from words into equations
@@ -128,19 +130,13 @@ class InfluenceDiagram(Scene):
         self.play(new_group.animate.shift(LEFT), complete_arrow.animate.shift(RIGHT * 0.7 + LEFT))
         self.wait(1)
 
-        # Make a group with everything
-        new_group += complete_arrow
-
-
         middle_of_spring_nodeF = new_group[1].get_center() + RIGHT*3
         # REMEMBER: this is the component model, no equations yet. 
-        spring_damping = MathTex(r"{{Spring}}\;Damping", font_size =35)
-        self.play(Write(spring_damping.move_to(middle_of_spring_nodeF)))
-        spring_velocity_eq = MathTex(r"{{Spring}}\; \vec{v}", font_size=35).move_to(middle_of_spring_nodeF)
+        spring_damping = MathTex(r"{{Spring}}\;Damping", font_size =35).move_to(middle_of_spring_nodeF)
+        self.play(Write(spring_damping))
         self.wait(1)
-        self.play(Transform(spring_damping, spring_velocity_eq))
+        spring_velocity_eq = MathTex(r"{{Spring}}\; \vec{v}", font_size=35).move_to(middle_of_spring_nodeF)
 
-        self.next_section()
 
         arrow1 = Arrow(
             new_group[0].get_right(), 
@@ -159,10 +155,168 @@ class InfluenceDiagram(Scene):
             color=BLUE_E,
         )
 
-        self.play(Create(arrow1))
-        self.play(Create(arrow2))
+
+        # make a group from the spring and two other arrows and remove for now (coming back to later)
+        damping_group = VGroup(arrow1, spring_velocity_eq, arrow2)
+        animations = [
+            Transform(spring_damping, spring_velocity_eq, replace_mobject_with_target_in_scene=True),
+            Create(arrow1),
+            Create(arrow2)
+        ]
+
+        for anim in animations:
+            self.play(anim)
+
+        
 
         self.wait(1)
+
+        # show constants that have not been covered
+        unshown_constants =  MathTex(
+            r"\text{mass} (m)\\ \text{spring constant} (k)\\ \text{damping coefficient} (c)\\ \text{original spring length} (L_o)",
+            font_size =35,
+        ).set_color(GREEN)
+        self.play(Write(unshown_constants.shift(DOWN*2).align_on_border(RIGHT)))
+        self.wait(1)
+
+
+        # remove the spring damping to talk about other stuff, move the network graph over.
+        self.play(FadeOut(damping_group, target_position=damping_group.get_center() + RIGHT*5),
+                  new_group.animate.align_on_border(LEFT))
+
+
+        
+        self.wait(1)
+
+        # display hookes law
+        hookes_law = MathTex(
+            r"{{Node\;\vec{F}}} = -k \, \Delta x",
+            font_size = 35
+        ).move_to(new_group[2]).align_to(new_group[2], LEFT)
+        print("HEREEEEEEEEEEE",hookes_law.get_center())
+        self.play(Indicate(new_group[2], run_time=2))
+
+        # THE BELOW WONT WORK, it keeps moving from the center which I don't really like
+        # so just write over and then replace??
+        # self.play(ReplacementTransform(new_group[2], hookes_law))
+        self.play(Write(hookes_law))
+        self.replace(new_group[2], hookes_law)
+
+        self.next_section() #FIXME:
+        self.wait(1)
+
+
+
+        # change in spring length
+        original_str = r"{{Spring\;\Delta x}}"
+        eq0 = MathTex(
+            original_str,
+            font_size = 35,
+        ).move_to(new_group[0], LEFT).align_to(new_group[0], LEFT)
+        eq1 = MathTex(
+            original_str + r"{{ = \text{original spring length} -}} \text{current spring length}",
+            font_size = 35,
+        ).move_to( new_group[0].get_center()).align_to(new_group[0], LEFT)
+        eq2 = MathTex(
+            original_str + r"{{ = L_o -}} \lVert\vec{x}_{node2} - \vec{x}_{node1}\rVert",
+            font_size = 35,
+        ).move_to( new_group[0].get_center()).align_to(new_group[0], LEFT)
+
+        # this to prevent some weird bs where it thought the strings were different
+        # so it would apply a transform information of the same object into itself
+        self.remove(new_group[0])
+        new_group[0] = eq0 
+        self.play(Indicate(new_group[0], run_time=2))
+        self.play(Write(eq1))
+        self.wait(1)
+        self.play(ReplacementTransform(eq1, eq2))
+        new_group[0] = eq2
+        self.wait(1)
+        
+        # self.add(index_labels(new_group).shift(RIGHT))
+        # self.add(index_labels(new_group[0][2]).shift(DOWN))
+        rec1 = SurroundingRectangle(new_group[0][2][1:8])
+        rec2 = SurroundingRectangle(new_group[0][2][9:16])
+        # self.play(Create(rec1), Create(rec2))
+        # remaking the text because surround isnt working when its in the group
+        rec3 = SurroundingRectangle(MathTex(r"Node \; x", font_size = 35).move_to(new_group[8]))
+        self.play(Create(rec3))
+        self.play(TransformFromCopy(rec3, rec1), Transform(rec3, rec2))
+        self.wait(1)
+        self.play(FadeOut(rec3, rec2, rec1))
+        self.wait(1)
+
+
+
+        fma = MathTex(r"\vec{F}",  r"=",  r"m", r"\vec{a}")
+        self.play(Write(fma), Flash(fma, flash_radius=fma.get_right()[0]+0.2, run_time = 2, line_length=0.6))
+        afm = MathTex(r"\vec{a}", r"=", r"{" ,  r"\vec{F}", r"\over", r"m", r"}")
+        self.wait(0.5)
+        self.play(TransformMatchingTex(fma, afm))
+        self.wait(1)
+
+
+        return
+
+
+        original_str = r"{{Spring \; \Delta \vec{x}}}"
+        eq0 = MathTex(
+            original_str,
+            font_size = 35,
+        ).move_to(new_group[0], LEFT).align_to(new_group[0], LEFT)
+        eq1 = MathTex(
+            original_str + r"{{ = \text{original spring length} -}} \text{current spring length}",
+            font_size = 35,
+        ).move_to( new_group[0].get_center()).align_to(new_group[0], LEFT)
+        eq2 = MathTex(
+            original_str + r"{{ = \text{original spring length} -}} (node_2 - node_1)",
+            font_size = 35,
+        ).move_to( new_group[0].get_center()).align_to(new_group[0], LEFT)
+
+
+        # this to prevent some weird bs where it thought the strings were different
+        # so it would apply a trasnform information of the same object into itself
+        self.remove(new_group[0])
+        new_group[0] = eq0 
+        # self.play(Transform(new_group[0], eq0, replace_mobject_with_target_in_scene=True))
+        # self.wait(1)
+        self.play(Transform(new_group[0], eq1, replace_mobject_with_target_in_scene=True))
+        self.wait(1)
+
+
+        self.play(Transform(eq1, eq2, replace_mobject_with_target_in_scene=True))
+
+
+
+        return 
+
+        test1 = MathTex(r"{{Spring \; \Delta \vec{x}}}",
+                        font_size=35)
+
+        test2 = MathTex(r"{{Spring \; \Delta \vec{x}}} = x + 3 -4 * a + \text{current spring length}",
+                        font_size= 35).align_to(test1, LEFT)
+        
+        test3 = MathTex(r"{{Spring \; \Delta \vec{x}}} = x + 3 -4 * a + (node_2 - node_1)",
+                        font_size= 35).align_to(test1, LEFT)
+        
+        
+        self.play(Write(test1))
+        self.wait(1)
+        self.play(Transform(test1,test2, replace_mobject_with_target_in_scene=True))
+        self.wait(1)
+        self.play(TransformMatchingTex(test2, test3,replace_mobject_with_target_in_scene=True))
+        self.wait(1)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
